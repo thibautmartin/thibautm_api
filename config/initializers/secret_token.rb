b7e7b3b007cb1bde909aa32430fc472ea6a1da72
Steve <steve@gocardless.com>
2013-08-30 23:00:56 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThibautmApi::Application.config.secret_key_base = '6f370108e7f52714e4edeb921dcc0a0cca1b67444fdc0b968296765120ab8d230fdfea2a1f1d6c27f797eb15a5e0e25636dc810e9365c1dde4c3df852d7a7e93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThibautmApi::Application.config.session_store :cookie_store, key: '_thibautm_api_session'
